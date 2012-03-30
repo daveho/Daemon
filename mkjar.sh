989f5bd25bfe15d0dcf2629e13f5e92355fd163b
David Hovemeyer <david.hovemeyer@gmail.com>
2012-03-30 16:33:36 -0400

    fix minor bug

--- mkjar.sh
+++ mkjar.sh
@@ -1,5 +1,5 @@
 rm -f daemon.jar
 echo "Main-class: org.cloudcoder.daemon.example.ExampleDaemonController" \
 	> manifest.txt
-cd bin && jar cfm ../daemon.jar ../manifest.txt org
+(cd bin && jar cfm ../daemon.jar ../manifest.txt org)
 rm -f manifest.txt
