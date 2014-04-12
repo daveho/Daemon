bccf80c5ce29f03425079473e219cd3633039b1b
David Hovemeyer <david.hovemeyer@gmail.com>
2014-04-12 10:20:12 -0400

    make ExampleDaemon use an actual worker thread
    This is much more realistic: in the original example,
    only the command reading loop (in the generic daemon library
    code) was actually running.  All "real" daemons will have
    one or more threads that represent the ongoing work.

--- src/org/cloudcoder/daemon/example/ExampleDaemon.java
+++ src/org/cloudcoder/daemon/example/ExampleDaemon.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2012, David H. Hovemeyer <david.hovemeyer@gmail.com>
+// Copyright (c) 2012-2014, David H. Hovemeyer <david.hovemeyer@gmail.com>
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -20,22 +20,85 @@
 
 package org.cloudcoder.daemon.example;
 
+import java.util.concurrent.LinkedBlockingQueue;
+
 import org.cloudcoder.daemon.IDaemon;
 
+/**
+ * Example implementation of {@link IDaemon}.
+ * 
+ * @author David Hovemeyer
+ */
 public class ExampleDaemon implements IDaemon {
+	/**
+	 * This worker runnable represents the ongoing work to
+	 * be done by the daemon process.
+	 * In a real application, it would be doing something useful
+	 * such as listening for client requests, doing periodic
+	 * tasks, etc.  This implementation just waits for
+	 * commands to arrive via a queue.
+	 */
+	private class Worker implements Runnable {
+		@Override
+		public void run() {
+			while (!shutdown) {
+				try {
+					String command = commandQueue.take();
+					System.out.println("Received a command: " + command);
+					System.err.println("Same command, to stderr: " + command);
+				} catch (InterruptedException e) {
+					System.out.println("Worker thread interrupted, shutting down");
+				}
+			}
+		}
+	}
+	
+	private LinkedBlockingQueue<String> commandQueue;
+	private volatile boolean shutdown;
+	private Thread workerThread;
+	
+	/**
+	 * Constructor.
+	 */
+	public ExampleDaemon() {
+		commandQueue = new LinkedBlockingQueue<String>();
+		shutdown = false;
+		workerThread = new Thread(new Worker());
+	}
+	
 	@Override
 	public void start(String instanceName) {
 		System.out.println("Daemon (instance name=" + instanceName + ") is starting!");
+		
+		// start the worker thread
+		workerThread.start();
 	}
 	
 	@Override
 	public void handleCommand(String command) {
-		System.out.println("Received a command: " + command);
-		System.err.println("Same command, to stderr: " + command);
+		try {
+			// Send the command to the worker
+			commandQueue.put(command);
+		} catch (InterruptedException e) {
+			System.out.println("This should not happen");
+		}
 	}
 
 	@Override
 	public void shutdown() {
 		System.out.println("Daemon is shutting down!");
+		
+		// Let the worker thread know that it is time to shut down
+		shutdown = true;
+		workerThread.interrupt();
+		
+		// Wait for the worker thread to finish
+		System.out.println("Waiting for worker thread to finish...");
+		try {
+			workerThread.join();
+		} catch (InterruptedException e) {
+			System.out.println("This should not happen");
+		}
+		System.out.println("Worker thread finished");
 	}
 }
