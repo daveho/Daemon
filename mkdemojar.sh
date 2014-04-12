14f5171eebd03e5c4900812a631f44135a862285
David Hovemeyer <david.hovemeyer@gmail.com>
2014-04-12 10:29:37 -0400

    starting gh-pages branch

--- mkdemojar.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-rm -f demo.jar
-echo "Main-class: org.cloudcoder.daemon.example.ExampleDaemonController" \
-	> manifest.txt
-(cd bin && jar cfm ../demo.jar ../manifest.txt org)
-rm -f manifest.txt
